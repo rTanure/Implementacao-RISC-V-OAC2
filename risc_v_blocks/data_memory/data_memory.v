7ab3a9eb00d3a7d2083742f23dcd17d1333e3411
Richardy Tanure <richardy.tanure@hotmail.com>
2025-07-28 10:59:02 -0300

    feat: Altera a memória de dados para salvar 8 bits

--- risc_v_blocks/data_memory/data_memory.v
+++ risc_v_blocks/data_memory/data_memory.v
@@ -5,18 +5,18 @@ module data_memory (
   input MemWrite,
   input MemRead,
 
-  input [`ADDR_WIDTH-1:0] address,
-  input [`DATA_WIDTH-1:0] write_data,
+  input [31:0] address,
+  input [7:0] write_data,
 
-  output [`DATA_WIDTH-1:0] read_data
+  output [7:0] read_data
 );
-  reg [`DATA_WIDTH-1:0] memory[`DMEM_DEPTH-1:0];
+  reg [7:0] memory[255:0];
 
-  assign read_data = memory[address >> 2'd2];
+  assign read_data = memory[address];
 
   always @(posedge clk) begin
     if (MemWrite) begin
-      memory[address >> 2'd2] <= write_data;
+      memory[address] <= write_data;
     end
   end
 endmodule
--- risc_v_blocks/data_memory/data_memory_tb.v
+++ risc_v_blocks/data_memory/data_memory_tb.v
@@ -7,10 +7,10 @@ module data_memory_tb;
 
   reg MemWrite;
   reg MemRead;
-  reg [`ADDR_WIDTH-1:0] address;
-  reg [`DATA_WIDTH-1:0] write_data;
+  reg [31:0] address;
+  reg [7:0] write_data;
 
-  wire [`DATA_WIDTH-1:0] read_data;
+  wire [7:0] read_data;
 
   data_memory uut (
     .clk(clk),
@@ -28,22 +28,22 @@ module data_memory_tb;
     MemWrite = 0;
     MemRead = 0;
     address = 32'd0;
-    write_data = 32'd0;
+    write_data = 8'd0;
     
     #10
     
     // Salvando dados na memoria
-    MemWrite = 1'b1;
+    MemWrite = 1;
 
     address = 32'd4;
-    write_data = 32'hAAAAAAAA;
+    write_data = 8'hAA;
     #10
 
-    address = 32'd8;
-    write_data = 32'hBBBBBBBB;
+    address = 32'd6;
+    write_data = 8'hBB;
     #10
     
-    MemWrite = 1'b0;
+    MemWrite = 0;
     #10
 
     // Lendo dados da memoria
@@ -51,11 +51,11 @@ module data_memory_tb;
     
     address = 32'd4;
     #5;
-    $display("esperado: %d, Valor: %h, esperado: AAAAAAAA)", address, read_data);
+    $display("esperado: %d, Valor: %h, esperado: AA)", address, read_data);
 
-    address = 32'd8;
+    address = 32'd6;
     #5;
-    $display("esperado: %d, Valor: %h, esperado: BBBBBBBB)", address, read_data);
+    $display("esperado: %d, Valor: %h, esperado: BB)", address, read_data);
 
     address = 32'd12;
     #5;
