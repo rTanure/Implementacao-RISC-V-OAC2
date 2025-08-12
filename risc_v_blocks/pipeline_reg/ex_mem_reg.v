9b5f82dfa269aa756c127ae1b6e1cfbbc9193f9b
PedroAlvesPaula <pedropaula2325p@gmail.com>
2025-08-12 13:18:37 -0300

    refact: tira linha vazia

--- risc_v_blocks/pipeline_reg/ex_mem_reg.v
+++ risc_v_blocks/pipeline_reg/ex_mem_reg.v
@@ -1,33 +1,27 @@
 module ex_mem_reg (
   input clk,
   input rst,
-
   input branch_in,
   input mem_read_in,
   input mem_to_reg_in,
   input mem_write_in,
   input reg_write_in,
-
   input [31:0] sum_pc_in,
   input [31:0] alu_result_in,
   input alu_zero_in,
   input [31:0] read_data_b_in,
   input [4:0] rd_in,
-
   input [31:0] instruction_in,
-
   output reg branch_out,
   output reg mem_read_out,
   output reg mem_to_reg_out,
   output reg mem_write_out,
   output reg reg_write_out,
   output reg [4:0] rd_out,
-
   output reg [31:0] sum_pc_out,
   output reg [31:0] alu_result_out,
   output reg alu_zero_out,
   output reg [31:0] read_data_b_out,
-
   output reg [31:0] instruction_out
 );
   always @(posedge clk or posedge rst) begin
